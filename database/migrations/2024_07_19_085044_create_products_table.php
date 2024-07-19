f5b12e03fb4c865057c8c9edbba98cb49f2e07cf
makara3 <makara@xolab-limited.com>
2024-07-19 16:56:28 +0700

    [Add]Add home page

--- database/migrations/2024_07_19_085044_create_products_table.php
+++ database/migrations/2024_07_19_085044_create_products_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->decimal('price', 10, 2);
             $table->integer('stock');
             $table->string('image');
+            $table->string('color')->nullable();
             $table->timestamps();
             
             $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,3 @@
 <x-layout>
-    <h1>
-        Hello From The Home Page!
-    </h1>
+
 </x-layout>
